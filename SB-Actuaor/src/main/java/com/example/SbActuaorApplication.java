5d2c68e9ee031f5734f0eca2a32e2c7c38e2d54f
suraj <surajphunde1234@gmail.com>
2025-08-15 17:50:03 +0530

    committed for SB-Actuator App
    Signed-off-by: suraj <surajphunde1234@gmail.com>

---
+++ SB-Actuaor/src/main/java/com/example/SbActuaorApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbActuaorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbActuaorApplication.class, args);
+	}
+
+}
